dd608657362f65d0f6bb886af14f7f5b69818160
xz-PC\xz <468531247@qq.com>
2017-09-22 17:19:23 +0800

    时间

--- common/src/main/java/com/xz/momo/common/date/DateUtils.java
+++ common/src/main/java/com/xz/momo/common/date/DateUtils.java
@@ -123,24 +123,5 @@ public class DateUtils {
         return false;
     }
 
-    /**
-     * 枚举类型，所有的时间格式
-     */
-    public enum Format {
 
-        YMD("yyyy-MM-dd"),
-        HMS("HH:mm:ss"),
-        YMDHM("yyyy-MM-dd HH:mm"),
-        YMDHMS("yyyy-MM-dd HH:mm:ss");
-
-        private String format;
-
-        Format(String format) {
-            this.format = format;
-        }
-
-        public String getFormat() {
-            return format;
-        }
-    }
 }
